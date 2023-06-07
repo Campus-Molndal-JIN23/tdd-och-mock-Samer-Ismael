3a5d0dc505c0f6c4c0d24a856fb52d4f94bcc87b
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-06-07 12:59:01 +0200

    Adding test Class

--- TDDproject/src/test/java/org/campusmolndal/demo/WeatherServiceTest.java
+++ TDDproject/src/test/java/org/campusmolndal/demo/WeatherServiceTest.java
@@ -1,20 +1,38 @@
 package org.campusmolndal.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class WeatherServiceTest {
 
+    private WeatherService sut;
+    private WeatherAPI mockAPI;
+
+    @BeforeEach
+    public void setup() {
+        mockAPI = mock(WeatherAPI.class);
+        sut = new WeatherService(mockAPI);
+    }
+
     @Test
     void getTemperature() {
+        when(mockAPI.getTemperature()).thenReturn(50.0);
+        assertEquals(50.0, sut.getTemperature());
     }
 
     @Test
     void getWindSpeed() {
+        when(mockAPI.getWindSpeed()).thenReturn(5.0);
+        assertEquals(5.0, sut.getWindSpeed());
     }
 
     @Test
     void getCloudiness() {
+        when(mockAPI.getCloudiness()).thenReturn(75.0);
+        assertEquals(75.0, sut.getCloudiness());
     }
 }
