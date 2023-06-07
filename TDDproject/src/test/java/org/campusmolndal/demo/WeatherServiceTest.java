8c46813ea416cd5d02897ab632dfddd9160c946a
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-06-07 14:23:38 +0200

    Using more assert

--- TDDproject/src/test/java/org/campusmolndal/demo/WeatherServiceTest.java
+++ TDDproject/src/test/java/org/campusmolndal/demo/WeatherServiceTest.java
@@ -22,17 +22,27 @@ class WeatherServiceTest {
     void getTemperature() {
         when(mockAPI.getTemperature()).thenReturn(50.0);
         assertEquals(50.0, sut.getTemperature());
+        assertNotEquals(0.0, sut.getTemperature());
+        assertTrue(sut.getTemperature() > 0);
+        assertFalse(sut.getTemperature() < 0);
     }
 
     @Test
     void getWindSpeed() {
         when(mockAPI.getWindSpeed()).thenReturn(5.0);
         assertEquals(5.0, sut.getWindSpeed());
+        assertNotEquals(0.0, sut.getWindSpeed());
+        assertTrue(sut.getWindSpeed() > 0);
+        assertFalse(sut.getWindSpeed() < 0);
+
     }
 
     @Test
     void getCloudiness() {
         when(mockAPI.getCloudiness()).thenReturn(75.0);
         assertEquals(75.0, sut.getCloudiness());
+        assertNotEquals(0.0, sut.getCloudiness());
+        assertTrue(sut.getCloudiness() > 0);
+        assertFalse(sut.getCloudiness() < 0);
     }
 }
